c0d0256e564e8476e3325ee6da38e5dd0252e1e2
riku10517 <furukawa.riku@nw.rikuden.co.jp>
2024-01-05 16:24:11 +0900

    Cokie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,13 @@ import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
 import '@/styles/globals.css';
+import { setCookie } from 'nookies'  // 追加
 
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
+  setCookie(null, 'key', 'sample');  // 追加
 
   return (
     <div className={inter.className}>
