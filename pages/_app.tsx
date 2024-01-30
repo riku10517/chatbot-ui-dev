bf4faaca4853934caa852a652d3428460335afc6
riku10517 <furukawa.riku@nw.rikuden.co.jp>
2024-01-30 14:57:57 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,36 @@ import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
 import '@/styles/globals.css';
-import { setCookie } from 'nookies'  // 追加
 
-const cookieid = new Date().getTime().toString(); // 追加(Cookieidを作成)
-// console.log( cookieid ); // 追加
+// ↓ 追加コード------------------------------------------------------------
+import { v4 as uuidv4 } from 'uuid'; 
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import { useEffect } from 'react';
+//------------------------------------------------------------------------
 
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
-  setCookie(null, 'ai_user', cookieid);  // 追加
+
+  // ↓ 追加コード------------------------------------------------------------
+  const [cookies, setCookie] = useCookies(['ai_user']); //const[現在値,更新用関数] = useCookies(['name'])
+  useEffect(() => {
+    if (!cookies['ai_user']) {
+      const cookieId = uuidv4();
+      const cookieExpiration = 31536000   //Cookie有効期限：1年 (60*60*24*365)
+      setCookie('ai_user', cookieId, { maxAge : cookieExpiration }); //setCookie('name','value',{option})
+      console.log('Cookie発行') 
+    }
+  }, [cookies]);
+  //------------------------------------------------------------------------
 
   return (
     <div className={inter.className}>
       <Toaster />
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+         <Component {...pageProps} />
       </QueryClientProvider>
     </div>
   );
