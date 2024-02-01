512f835590f9402eaac1bd33e79dcccf33124af6
riku10517 <furukawa.riku@nw.rikuden.co.jp>
2024-02-01 14:28:44 +0900

    “Cookie設定/取得/更加”

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
-  reactStrictMode: true,
+  reactStrictMode: true, 
+  // reactStrictMode: false, //初回useEffectが２回呼び出されるため無効化
 
   webpack(config, { isServer, dev }) {
     config.experiments = {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,17 +19,31 @@ const inter = Inter({ subsets: ['latin'] });
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
 
-  // ↓ 追加コード------------------------------------------------------------
+  // ↓ 追加コード------------------------------------------------------------------------------------------------
   const [cookies, setCookie] = useCookies(['ai_user']); //const[現在値,更新用関数] = useCookies(['name'])
+
+   // 初回ログイン（Cookie取得）
   useEffect(() => {
     if (!cookies['ai_user']) {
       const cookieId = uuidv4();
-      const cookieExpiration = 31536000   //Cookie有効期限：1年 (60*60*24*365)
+      const cookieExpiration = 31536000;   //Cookie有効期限：1年 (60*60*24*365)
       setCookie('ai_user', cookieId, { maxAge : cookieExpiration }); //setCookie('name','value',{option})
-      console.log('Cookie発行') 
+      console.log('Cookie発行') ;
+      console.log('初回',cookieId);
     }
-  }, [cookies]);
-  //------------------------------------------------------------------------
+  }, [cookies]); //依存配列=[cookies]：Cookie削除によるCookie再発行
+
+    // 2回目以降ログイン（Cookie有効期限の更新）
+  useEffect(() => {
+    if (cookies['ai_user']) {
+      const cookieId = cookies['ai_user'];
+      const cookieExpirationUpdate = 31536000;  //Cookie更新年数：1年
+      setCookie('ai_user', cookieId, { maxAge : cookieExpirationUpdate }); //Cookie有効期限を再セット
+      console.log('Cookie更新'); 
+      console.log('更新',cookieId);
+    }
+  }, []); //依存配列=[]：初回レンダリングでのみ実行
+  //---------------------------------------------------------------------------------------------------------------
 
   return (
     <div className={inter.className}>
