94d0097fc59b532b08edb1fb0fe2ed51454f202b
riku10517 <furukawa.riku@nw.rikuden.co.jp>
2024-02-02 13:53:24 +0900

    💄 Chatbot-UIのGitソースリンク表示を削除

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -377,7 +377,7 @@ export const ChatInput = ({
           )}
         </div>
       </div>
-      {/* ↓ nwrikuden-Chatbot Gitソースコードリンク不要のためコメントアウト
+      {/* ↓ nwrikuden-Chatbot Gitソースコードリンクをコメントアウト
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
           href="https://github.com/mckaywrigley/chatbot-ui"
